3113e94b2cdf33123cbaae94f2b4c87f2c1023b4
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2024-09-28 12:34:58 -0700

    Adding typescript

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let addBtn = document.querySelector(".addBtn");
+let delAllBtn = document.querySelector(".delAll");
+// console.log(addBtn);
+// console.log(delAllBtn);
+// console.log("Hello World!");
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+let addBtn = document.querySelector(".addBtn") as HTMLButtonElement;
+let delAllBtn = document.querySelector(".delAll") as HTMLButtonElement;
+// console.log(addBtn);
+// console.log(delAllBtn);
+console.log("Hello World!");
+
+
